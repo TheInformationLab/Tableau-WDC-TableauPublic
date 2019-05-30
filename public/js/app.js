9d38a58858a7fd9c09b3890c6a7231767cd34cc6
andre347 <aedevries19@gmail.com>
2019-05-30 16:36:42 +0100

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,89 @@
+(function() {
+  // Create the connector object
+  var myConnector = tableau.makeConnector();
+
+  function convertDate(d) {
+    var s = new Date(d);
+    return s;
+  }
+
+  // Define the schema
+  myConnector.getSchema = function(schemaCallback) {
+    var tableSchema = {
+      id: "TableauPublic",
+      alias: "Tableau Public API",
+      columns: cols
+    };
+
+    schemaCallback([tableSchema]);
+  };
+
+  // Grab data from server /data
+  myConnector.getData = function(table, doneCallback) {
+    // var url = "http://localhost:3001" + "/data";
+    var deployedUrl = "https://tableau-public-api.wdc.dev" + "/data";
+    var sendUsername = tableau.connectionData.replace(/\s/g, "");
+
+    $.getJSON(deployedUrl, {
+      data: sendUsername,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json"
+    }).done(function(resp) {
+      var tableData = [];
+
+      for (var i = 0, len = resp.length; i < len; i++) {
+        tableData.push({
+          title: resp[i].title,
+          category: resp[i].category,
+          authorProfileName: resp[i].authorProfileName,
+          ownerId: resp[i].ownerId,
+          workbookRepoUrl: resp[i].workbookRepoUrl,
+          numberOfFavorites: resp[i].numberOfFavorites,
+          firstPublishDate: convertDate(resp[i].firstPublishDate),
+          lastPublishDate: convertDate(resp[i].lastPublishDate),
+          lastUpdateDate: convertDate(resp[i].lastUpdateDate),
+          permalink: resp[i].permalink,
+          viewCount: resp[i].viewCount,
+          showTabs: resp[i].showTabs,
+          showToolbar: resp[i].showToolbar,
+          showByline: resp[i].showByline,
+          warnDataAccess: resp[i].warnDataAccess,
+          showShareOptions: resp[i].showShareOptions,
+          showWatermark: resp[i].showWatermark,
+          allowDataAccess: resp[i].allowDataAccess,
+          defaultViewName: resp[i].defaultViewName,
+          size: resp[i].size,
+          revision: resp[i].revision,
+          extractInfo: resp[i].extractInfo,
+          defaultViewRepoUrl: resp[i].defaultViewRepoUrl,
+          description: resp[i].description,
+          attributionsauthorProfileName: resp[i].attributionsauthorProfileName,
+          attributionsattributionUrl: resp[i].attributionsattributionUrl,
+          attributionsworkbookRepoUrl: resp[i].attributionsworkbookRepoUrl,
+          attributionsauthorDisplayName: resp[i].attributionsauthorDisplayName,
+          attributionsworkbookName: resp[i].attributionsworkbookName,
+          attributionsworkbookViewName: resp[i].attributionsworkbookViewName
+        });
+      }
+      table.appendRows(tableData);
+      doneCallback();
+    });
+  };
+
+  tableau.registerConnector(myConnector);
+
+  // Create event listeners for when the user submits the form
+  $(document).ready(function() {
+    $("#submitButton").click(function() {
+      var userName = $("#tableauUsername").val();
+      if (userName !== "") {
+        tableau.connectionName = "Tableau Public API"; // This will be the data source name in Tableau
+        tableau.connectionData = $("#tableauUsername").val(); // The Username in Tableau Public
+        tableau.submit(); // This sends the connector object to Tableau
+      } else {
+        document.getElementById("error").innerHTML = "Please enter a username";
+        tableau.log("No username entered..");
+      }
+    });
+  });
+})();
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+const flatten = require("flat");
+const cors = require("cors");
+
+app.use(express.json());
+
+let index = 0;
+
+//enable cors for only our WDC front-end
+const corsOptions = {
+  origin: "https://tableau-public.wdc.dev",
+  optionsSuccessStatus: 200
+};
+
+// get data
+const getData = async (user, arr) => {
+  try {
+    let result = arr || [];
+    const count = 100;
+    const res = await fetch(`https://public.tableau.com/profile/api/${user}/workbooks?count=${count}&index=${index}`);
+    const json = await res.json();
+    json.map(viz => {
+      const flatViz = flatten(viz);
+      let str = JSON.stringify(flatViz).replace(/\.\d\./g, "");
+      newJson = JSON.parse(str);
+      result.push(newJson);
+    });
+    console.log(`Initial JSON length: ${json.length}`);
+    if (json.length >= count) {
+      index += 100;
+      console.log(`Got more data: ${index} - Count: ${count}`);
+      await getData(user, result);
+    }
+    index = 0;
+    return await result;
+  } catch (err) {
+    // logging errors if there are any
+    console.log(err);
+    return err;
+  }
+};
+
+// function to loop over array with promise
+const getAllUserData = async array => {
+  return await Promise.all(array.map(user => getData(user)));
+};
+
+// create route to get data
+app.get("/data", cors(corsOptions), async (req, res) => {
+  let returnedDataArr = [];
+
+  const username = req.query.data;
+  const usernameArray = username.split(",");
+  const returnedData = await getAllUserData(usernameArray);
+
+  returnedData.map(userdata => {
+    returnedDataArr.push(...userdata);
+  });
+
+  //   const usertoFetch = usernameArray[0];
+  //   const data = await getData(usertoFetch);
+  res.json(returnedDataArr);
+
+  return;
+});
+
+app.listen();
