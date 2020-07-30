38d3ea2ee877aab47288f86f1ff775418c632553
andre347 <aedevries19@gmail.com>
2020-07-30 16:41:24 +0100

    FIX: set up CORS for serverless function

--- api/index.js
+++ api/index.js
@@ -1,6 +1,28 @@
 const fetch = require("node-fetch");
 const flatten = require("flat");
 
+// CORS setting for Vercel
+// Source: https://vercel.com/knowledge/how-to-enable-cors
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another option
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+// index for looping
+
 let index = 0;
 
 // get data
