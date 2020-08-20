c54bb302c9a466a12c44f3182afe0860ced26449
andre347 <aedevries19@gmail.com>
2020-08-20 11:13:35 +0100

    BUG: changed the schema to match date instead of datetime

--- public/js/schema.js
+++ public/js/schema.js
@@ -1,134 +1,134 @@
 var cols = [
   {
     id: "category",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "authorProfileName",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "ownerId",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "workbookRepoUrl",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "numberOfFavorites",
-    dataType: tableau.dataTypeEnum.int
+    dataType: tableau.dataTypeEnum.int,
   },
   {
     id: "firstPublishDate",
-    dataType: tableau.dataTypeEnum.datetime
+    dataType: tableau.dataTypeEnum.date,
   },
   {
     id: "lastPublishDate",
-    dataType: tableau.dataTypeEnum.datetime
+    dataType: tableau.dataTypeEnum.date,
   },
   {
     id: "lastUpdateDate",
-    dataType: tableau.dataTypeEnum.datetime
+    dataType: tableau.dataTypeEnum.date,
   },
   {
     id: "description",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "showTabs",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "showToolbar",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "showByline",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "showShareOptions",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "allowDataAccess",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "warnDataAccess",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "showWatermark",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "defaultViewName",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "setDefaultViewName",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "setOwnerId",
-    dataType: tableau.dataTypeEnum.bool
+    dataType: tableau.dataTypeEnum.bool,
   },
   {
     id: "permalink",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "title",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "viewCount",
-    dataType: tableau.dataTypeEnum.int
+    dataType: tableau.dataTypeEnum.int,
   },
   {
     id: "size",
-    dataType: tableau.dataTypeEnum.int
+    dataType: tableau.dataTypeEnum.int,
   },
   {
     id: "defaultViewRepoUrl",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "revision",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "attributionsauthorProfileName",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "attributionsattributionUrl",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "attributionsworkbookRepoUrl",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "attributionsauthorDisplayName",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "attributionsworkbookName",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "attributionsworkbookViewName",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "extractInfo",
-    dataType: tableau.dataTypeEnum.string
+    dataType: tableau.dataTypeEnum.string,
   },
   {
     id: "showInProfile",
-    dataType: tableau.dataTypeEnum.bool
-  }
+    dataType: tableau.dataTypeEnum.bool,
+  },
 ];
